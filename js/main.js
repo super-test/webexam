e8d38f9f35cd099dbfe9ad12a84d2a1873195bb3
aLLenka <rikhnavskaja.elena@gmail.com>
2015-08-26 15:46:38 +0200

    html tags with button are working

--- js/main.js
+++ js/main.js
@@ -143,6 +143,7 @@ window.onload = (function() {
 
 	var output = document.getElementById('fileOutput');
 	var input = document.getElementById('fileInput');
+	var boldButton = document.getElementById('boldButton')
 	output.focus();
 
 	var requestedBytes = 3 * 1024 * 1024; // 3MB
@@ -220,12 +221,70 @@ window.onload = (function() {
     	}, errorHandler);
 
 	};
+	
+	var insert_tag = function(_obj_name, _tag_start, _tag_end)
+		// _obj_name - name объекта - как правило, textarea, но при желании можно сделать любой
+		// указываем именно NAME, так как согласно стандартам DOCTYPE HTML 4.01 strict и выше
+		// свойство ID у объектов ввода является не приемлемым и требуется обращаться только name
+		// _tag_start - что вставлять перед выделенным текстом
+		// _tag_end - что вставлять после выделенного текста
+		{
+			alert('start');
+			// берем объект
+			var area=document.getElementsByName(_obj_name).item(0);
+
+			// Mozilla и другие НОРМАЛЬНЫЕ браузеры
+			// ЕСЛИ есть что-либо выделенное, ТО
+			if (document.getSelection)
+			{// берем все, что до выделения
+			 area.value=area.value.substring(0,area.selectionStart)+
+
+			 // вставляем стартовый тег
+			 _tag_start+
+
+			 // вставляем выделенный текст
+			 area.value.substring(area.selectionStart, area.selectionEnd)+
+
+			 // вставляем закрывающий тег
+			 _tag_end+
+
+			 // вставляем все, что после выделения
+			 area.value.substring(area.selectionEnd,area.value.length);
+			}
+
+			// Иначе заплатка для Internet Explorer
+			else
+			{// берем текст
+			 var selectedText=document.selection.createRange().text;
+			 // ЕСЛИ имеется какой-то выделенный текст, ТО
+			 if (selectedText!='')
+			 {// составляем новый текст
+			  var newText=_tag_start+selectedText+_tag_end;
+			  // вставляем новый текст
+			  document.selection.createRange().text=newText;
+			}
+			}
+		}// /insert_tag
+
+		/**
+		 * Обертка для обработчика, чтобы передать параметры
+		 *
+		 * @method     addParam
+		 * @return     {function} Возвращает обработчик с заданными параметрами
+		 */
+		var addInsertTag = function() {
+			return insert_tag("edit", "<b>", "</b>"); 
+		};
+
+
+		//boldButton.onclick = "insert_tag('edit','<strong>','</strong>');";
+		boldButton.onclick = addInsertTag;
 
 		createFileButton.onclick = createFiles;
 		readFileButton.onclick = readFiles;
 		writeFileButton.onclick = writeFiles;
 		input.oninput = new Function("document.getElementById('fileOutput').innerHTML = this.value");
-		
+
 	}
 
 	
